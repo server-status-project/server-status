328afe1c800a049525c7a3f2f0d9b027cad88742
Vojtěch Sajdl <vojtech@sajdl.com>
2018-01-07 21:05:03 +0100

    SQL with foreign keys

--- install.sql
+++ install.sql
@@ -1,18 +1,15 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-CREATE TABLE IF NOT EXISTS `services` (
+CREATE TABLE `services` (
   `id` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8_czech_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-CREATE TABLE IF NOT EXISTS `services_status` (
+CREATE TABLE `services_status` (
   `id` int(11) NOT NULL,
   `service_id` int(11) NOT NULL,
   `status_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-CREATE TABLE IF NOT EXISTS `status` (
+CREATE TABLE `status` (
   `id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
   `title` varchar(50) COLLATE utf8_czech_ci NOT NULL,
@@ -22,14 +19,14 @@ CREATE TABLE IF NOT EXISTS `status` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-CREATE TABLE IF NOT EXISTS `tokens` (
+CREATE TABLE `tokens` (
   `token` varchar(64) COLLATE utf8_czech_ci NOT NULL,
   `user` int(11) NOT NULL,
   `expire` int(11) NOT NULL,
   `data` varchar(80) COLLATE utf8_czech_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(60) COLLATE utf8_czech_ci NOT NULL,
   `username` varchar(50) COLLATE utf8_czech_ci NOT NULL,
@@ -41,31 +38,44 @@ CREATE TABLE IF NOT EXISTS `users` (
   `active` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+
 ALTER TABLE `services`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `services_status`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `service_id` (`service_id`),
+  ADD KEY `status_id` (`status_id`);
 
 ALTER TABLE `status`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 ALTER TABLE `tokens`
-  ADD PRIMARY KEY (`token`);
+  ADD PRIMARY KEY (`token`),
+  ADD KEY `user` (`user`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `username` (`username`);
 
+
 ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `services_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `services_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `service_id` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`),
+  ADD CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
 
 ALTER TABLE `status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tokens`
+  ADD CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
