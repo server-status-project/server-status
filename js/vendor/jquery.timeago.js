184247c4abe777399a98f2711f47b2d6e1d5d57c
Richard Schwab <richard.schwab@valtech.com>
2020-05-11 07:07:02 +0200

    Update libs: - jquery to 3.2.1 - jquery.growl to 1.3.5 - jquery.timeago to 1.6.7 - bootstrap to 3.4.1

--- js/vendor/jquery.timeago.js
+++ js/vendor/jquery.timeago.js
@@ -3,15 +3,15 @@
  * updating fuzzy timestamps (e.g. "4 minutes ago" or "about 1 day ago").
  *
  * @name timeago
- * @version 1.6.1
- * @requires jQuery v1.2.3+
+ * @version 1.6.7
+ * @requires jQuery >=1.5.0 <4.0
  * @author Ryan McGeary
  * @license MIT License - http://www.opensource.org/licenses/mit-license.php
  *
  * For usage and examples, visit:
  * http://timeago.yarp.com/
  *
- * Copyright (c) 2008-2017, Ryan McGeary (ryan -[at]- mcgeary [*dot*] org)
+ * Copyright (c) 2008-2019, Ryan McGeary (ryan -[at]- mcgeary [*dot*] org)
  */
 
 (function (factory) {
@@ -42,7 +42,7 @@
     settings: {
       refreshMillis: 60000,
       allowPast: true,
-      allowFuture: true,
+      allowFuture: false,
       localeTitle: false,
       cutoff: 0,
       autoDispose: true,
@@ -51,7 +51,7 @@
         prefixFromNow: null,
         suffixAgo: "ago",
         suffixFromNow: "from now",
-        inPast: 'any moment now',
+        inPast: "any moment now",
         seconds: "less than a minute",
         minute: "about a minute",
         minutes: "%d minutes",
