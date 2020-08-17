169b165b5d5f10c3c7f98635c718bd2e3660da8a
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-08-17 16:52:20 +0300

    Create options.php

---
+++ admin/options.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!file_exists("../config.php"))
+{
+  header("Location: ../");
+}
+else{
+  require_once("../config.php");
+  require_once("../classes/constellation.php");
+  require_once("../classes/mailer.php");
+  require_once("../classes/notification.php");
+  require_once("../template.php");
+  require_once("../libs/parsedown/Parsedown.php");
+  require_once("../classes/queue.php");
+  require_once("../classes/db-class.php");
+  $db = new SSDB();
+  define("NAME", $db->getSetting($mysqli,"name"));
+  define("TITLE", $db->getSetting($mysqli,"title"));
+  define("WEB_URL", $db->getSetting($mysqli,"url"));
+  define("MAILER_NAME", $db->getSetting($mysqli,"mailer"));
+  define("MAILER_ADDRESS", $db->getSetting($mysqli,"mailer_email"));
+  Template::render_header(_("Options"), true);
+?>
+<div class="text-center">
+	<h2>Server Status Options</h2>
+</div>
