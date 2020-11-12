85dfce55c87cdc4b75be44ad680405dd015c3501
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-11-12 22:06:55 +0300

    Remove old version server checks

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ if (isset($_GET['subscriber_logout'])){
   unset($_SESSION['subscriber_id']);
   header('Location: index.php');
 }
+/*
 $versionfile = fopen("versionfile", "r") or die("Unable to open version file!");
 $appversion = fread($versionfile,filesize("versionfile"));
 fclose($versionfile);
@@ -90,6 +91,7 @@ if($db->getSetting($mysqli,"notifyUpdates") == "yes"){
     die("Your installation is not upp to date! Download the new update from: '".$remotedl."' Your version is:'".$appversion."' Remote Authority Version is:'".$remoteversion."' Your Update Seed is:'".$useed."' Remote Package Authority is Skyfallen. <br>If you cannot access Remote Authority, please check status.theskyfallen.com and skyfallenhosted.ml manually.");
   }
 }
+*/
 Template::render_header("Status");
 ?>
     <div class="text-center">
