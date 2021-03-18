19c35b59c650d09fe3c5ce177daf633907fe0847
Tealk <tealk@anzah.email>
2021-03-18 22:06:21 +0100

    redesign login form

--- admin/login-form.php
+++ admin/login-form.php
@@ -11,20 +11,30 @@ Template::render_header(_("Login"));
     <p class="alert alert-info"><?php echo _("Please login to continue."); ?></p>
   <?php } ?>
   <form action="<?php echo WEB_URL; ?>/admin/" method="post">
-    <div class="form-group">
-      <label for="email"><?php echo _("Email"); ?></label>
-      <input placeholder="<?php echo _("Email"); ?>" class="form-control" name="email" id="email" type="email" tabindex="1" value="<?php echo htmlspecialchars((isset($_POST['email']) ? $_POST['email'] : ''), ENT_QUOTES); ?>" required>
-    </div>
-    <div class="form-group" style="margin-bottom: 8px">
-      <label for="pass"><?php echo _("Password"); ?></label>
-      <input placeholder="<?php echo _("Password"); ?>" class="form-control" name="pass" id="pass" type="password" tabindex="2" required>
-      <div style="padding-top: 8px; position: relative;">
-        <a href="<?php echo WEB_URL; ?>/admin/?do=lost-password" class="pull-right noselect" tabindex="5"><?php echo _("Forgotten password?"); ?></a>
-        <input name="remember" id="remember" type="checkbox" tabindex="3"> <label class="lbl-login noselect" style="color: black;" for="remember"><?php echo _("Remember me"); ?></label>
+    <div class="card">
+      <div class="card-header">
+        <h1><?php echo _("Login"); ?></h1>
       </div>
-    </div>
-    <div class="form-group clearfix">
-      <button type="submit" class="btn btn-success pull-right" tabindex="4"><?php echo _("Login"); ?></button>
+      <div class="card-body">
+        <div class="input-group mb-3">
+          <span class="input-group-text"><i class="fas fa-at"></i></span>
+          <input type="email" id="email" name="email" class="form-control" placeholder="<?php echo _("Email"); ?>" value="<?php echo htmlspecialchars((isset($_POST['email']) ? $_POST['email'] : ''), ENT_QUOTES); ?>" required>
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text"><i class="fas fa-key"></i></span>
+          <input type="password" id="pass" name="pass" class="form-control" placeholder="<?php echo _("Password"); ?>" required>
+        </div>
+        <a href="<?php echo WEB_URL; ?>/admin/?do=lost-password" class="float-end" tabindex="5"><?php echo _("Forgotten password?"); ?></a>
+        <div class="input-group mb-3">
+          <div class="input-group-text nrbr">
+            <input type="checkbox" name="remember" id="remember">
+          </div>
+          <label class="input-group-append input-group-text nlbr nobg" for="remember"><?php echo _("Remember me"); ?></label>
+        </div>
+        <div class="form-group">
+          <input type="submit" value="<?php echo _("Login"); ?>" class="btn btn-success float-end">
+        </div>
+      </div>
     </div>
   </form>
 </div>
