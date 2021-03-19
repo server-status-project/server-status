c63c21212b413bf60e6ebfd2b04fbe2de2bcbfe3
Tealk <tealk@anzah.email>
2021-03-19 14:41:41 +0100

    group headline colore

--- classes/service.php
+++ classes/service.php
@@ -221,7 +221,7 @@ class Service implements JsonSerializable
       echo '<ul class="list-group components mt-3">';
       //echo '<ul class="platforms list-group mb-2">';
       // Render the group status if it exists
-      echo '<li class="list-group-item list-group-item-success group-name"><span><i class="fas fa-folder-open"></i></span>&nbsp;' . $this->group_name . '<div class="status text-' . $classes[$this->status] . '">' . _($statuses[$this->status]) . '</div></li>';
+      echo '<li class="list-group-item list-group-item-' . $classes[$this->status] . ' group-name"><span><i class="fas fa-folder-open"></i></span>&nbsp;' . $this->group_name . '<div class="status text-' . $classes[$this->status] . '">' . _($statuses[$this->status]) . '</div></li>';
       //echo '<li class="cist-group-item d-flex flex-row justify-content-between platform list-group-item-action py-0 expanded" role="button">' . $this->group_name .'<div class="status '. $classes[$this->status] .'"'. _($statuses[$this->status]).'</div></li>';
       $arrCompletedGroups[] = $this->group_name;
       $boolOpened = true;
