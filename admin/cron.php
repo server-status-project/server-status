1b1c234d66480e3bfc5a508b60ad7241d6729f83
Tealk <tealk@anzah.email>
2021-05-01 23:40:31 +0200

    rudimentary functions

--- admin/cron.php
+++ admin/cron.php
@@ -12,9 +12,11 @@ if (!file_exists("../config.php")) {
    * @param String $name name to the fetched url
    * @param Objekt $mysqli mysql connection info from config.php
    */
-  function check($url, $id, $name, $mysqli) {
+  function check($url, $id, $name, $mysqli)
+  {
     $status_time = time();
-    $status_user_id = "3";
+    $status_user_id = "1";
+    $status_type = "0";
     $ishttp = preg_match("/^http(s)?:/", $url);
     if ($ishttp) {
       $ch = curl_init();
@@ -23,32 +25,35 @@ if (!file_exists("../config.php")) {
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_TIMEOUT, 10);
       $http_respond = curl_exec($ch);
-      $http_respond = trim( strip_tags( $http_respond ) );
-      $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-      if ( ( $http_code >= "200" ) && ( $http_code < "300" ) ) {
+      $http_respond = trim(strip_tags($http_respond));
+      $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      if (($http_code >= "200") && ($http_code < "300")) {
         $status_text = "Running without Problems";
-        #if (lastOffline($mysqli, $id)) {
-        #  writeStatus($mysqli, $id, "3", "Online check", $status_text, time(), "0", $status_user_id);
-        #}
-        echo "<p style='color:green'>" . $id . " " . $name . "<br>RESPONDE: " . $status_text . "</p>";
+        echo "<p style='color:green'>" . $id . " " . $name;
+        if (lastOffline($mysqli, $status_type, $id, $status_user_id)) {
+          echo " <span style='color:orange'>lastOffline true</span>";
+          #  writeStatus($mysqli, $id, "3", "Online check", $status_text, time(), "0", $status_user_id);
+        }
+        echo "<br>RESPONDE: " . $status_text . "</p>";
       } else {
         $http_codes = parse_ini_file("http_codes.ini");
         $status_text = $http_code . " " . $http_codes[$http_code];
-        # writeStatus($mysqli, $id, "1", "Online check", $status_text, time(), "0", $status_user_id);
+        # writeStatus($mysqli, $id, "$status_type", "Online check", $status_text, time(), "0", $status_user_id);
         echo "<p style='color:red'>" . $id . " " . $name . "<br>RESPONDE: " . $status_text . "</p>";
       }
-      curl_close( $ch );
+      curl_close($ch);
     } else {
       list($ip, $port) = preg_split("/:/", $url);
       if (fsockopen($ip, $port)) {
         $status_text = "Running without Problems";
-        #if (lastOffline($mysqli, $id)) {
-        #  writeStatus($mysqli, $id, "3", "Online check", $status_text, time(), "0", $status_user_id);
-        #}
+        if (lastOffline($mysqli, $status_type, $id, $status_user_id)) {
+          echo " <span style='color:orange'>lastOffline true</span>";
+          #  writeStatus($mysqli, $id, "3", "Online check", $status_text, time(), "0", $status_user_id);
+        }
         echo "<p style='color:green'>" . $id . " " . $name . "<br>RESPONDE: " . $status_text . "</p>";
       } else {
         $status_text = "Can't reach the Server";
-        # writeStatus($mysqli, $id, "1", "Online check", $status_text, time(), "0", $status_user_id);
+        # writeStatus($mysqli, $id, "$status_type", "Online check", $status_text, time(), "0", $status_user_id);
         echo "<p style='color:red'>" . $id . " " . $name . "<br>RESPONDE: " . $status_text . "</p>";
       }
     }
@@ -83,15 +88,20 @@ if (!file_exists("../config.php")) {
   /**
    * Write the Status into DB
    * @param Objekt $mysqli mysql connection info from config.php
+   * @param int $status_type Error type 0: Major outage 1: Minor outage 2: Planned maintenance 3: Operational
    * @param int $status_service_id id of the service currently being checked
+   * @param int $status_user_id the User id
    */
-  function lastOffline($mysqli, $status_service_id)
+  function lastOffline($mysqli, $status_type, $status_service_id, $status_user_id)
   {
-    $status_id_type = $mysqli->query("SELECT 'status_id'.'type' FROM 'services_status'.'status' WHERE 'service_id' = $status_service_id AND 'status'.'id' = 'services_status'.'status_id' ORDER BY 'id' DESC limit 1");
-    if ($status_id_type == "1") {
-      return true;
-    } else {
-      return false;
+    $result = $mysqli->query("select status.type from status, services_status where services_status.service_id = '$status_service_id' AND status.id = services_status.status_id AND status.user_id = '$status_user_id' ORDER BY `status`.`id` DESC limit 1;");
+    $status_id_type = $result->fetch_row();
+    if (isset($status_id_type[0])) {
+      if ($status_id_type[0] == "$status_type") {
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 
