990aaa38e87bd099b91db2b0982975acaf6db4c2
Tealk <tealk@anzah.email>
2021-05-03 12:51:15 +0200

    Functions improved, maintenance mode testing
    functions improved to make it easier to implement enhancements

--- admin/cron.php
+++ admin/cron.php
@@ -4,20 +4,14 @@ if (!file_exists("../config.php")) {
 } else {
   require_once("../config.php");
   header('Cache-Control: no-cache');
+  $status_user_id = "1";
 
   /**
    * Check the status
    * @param String $url url or ip to check
-   * @param int $id id to the fetched url
-   * @param String $name name to the fetched url
-   * @param Objekt $mysqli mysql connection info from config.php
    */
-  function check($url, $id, $name, $mysqli)
+  function check($url)
   {
-    $status_time = time();
-    $status_user_id = "1";
-    $status_type_offline = "0";
-    $status_type_online = "3";
     $ishttp = preg_match("/^http(s)?:/", $url);
     if ($ishttp) {
       $ch = curl_init();
@@ -29,39 +23,56 @@ if (!file_exists("../config.php")) {
       $http_respond = trim(strip_tags($http_respond));
       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       if (($http_code >= "200") && ($http_code < "300")) {
-        $status_text = "Running without Problems";
-        echo "<p style='color:green'>" . $id . " " . $name;
-        if (lastOffline($mysqli, $status_type_offline, $id, $status_user_id)) {
-          echo " <span style='color:orange'>lastOffline true</span>";
-          writeStatus($mysqli, $id, "3", "Online check", $status_text, time(), "0", $status_user_id);
-        }
-        echo "<br>RESPONDE: " . $status_text . "</p>";
+        return array("url", true, $http_code);
       } else {
-        $http_codes = parse_ini_file("http_codes.ini");
-        $status_text = $http_code . " " . $http_codes[$http_code];
-        echo "<p style='color:red'>" . $id . " " . $name;
-        if (!lastOffline($mysqli, $status_type_offline, $id, $status_user_id)) {
-          echo " <span style='color:orange'>lastOffline true</span>";
-          writeStatus($mysqli, $id, "$status_type_offline", "Online check", $status_text, time(), "0", $status_user_id);
-        }
-        echo "<br>RESPONDE: " . $status_text . "</p>";
+        return array("url", false, $http_code);
       }
       curl_close($ch);
     } else {
       list($ip, $port) = preg_split("/:/", $url);
       if (fsockopen($ip, $port)) {
-        $status_text = "Running without Problems";
-        if (lastOffline($mysqli, $status_type_offline, $id, $status_user_id)) {
-          writeStatus($mysqli, $id, "3", "Online check", $status_text, time(), "0", $status_user_id);
-        }
-        echo "<p style='color:green'>" . $id . " " . $name . "<br>RESPONDE: " . $status_text . "</p>";
+        return array("ip", true, "");
       } else {
-        $status_text = "Can't reach the Server";
-        if (!lastOffline($mysqli, $status_type_offline, $id, $status_user_id)) {
-          writeStatus($mysqli, $id, "$status_type_offline", "Online check", $status_text, time(), "0", $status_user_id);
-        }
-        echo "<p style='color:red'>" . $id . " " . $name . "<br>RESPONDE: " . $status_text . "</p>";
+        return array("ip", false, "");
       }
+    }
+  }
+
+  /**
+   * Write the Status into DB
+   * @param Objekt $mysqli mysql connection info from config.php
+   * @param int $status_type Error type 0: Major outage 1: Minor outage 2: Planned maintenance 3: Operational
+   * @param int $status_service_id id of the service currently being checked
+   * @param int $status_user_id the User id
+   */
+  function lastOffline($mysqli, $status_service_id, $status_user_id)
+  {
+    $result = $mysqli->query("select status.type from status, services_status where services_status.service_id = '$status_service_id' AND status.id = services_status.status_id AND status.user_id = '$status_user_id' ORDER BY `status`.`id` DESC limit 1;");
+    $status_id_type = $result->fetch_row();
+    if (isset($status_id_type[0])) {
+      if ($status_id_type[0] == "0") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  /**
+   * Write the Status into DB
+   * @param Objekt $mysqli mysql connection info from config.php
+   * @param int $status_service_id id of the service currently being checked
+   * @param int $status_end_time time against which is checked (normally: current time)
+   */
+  function maintenance($mysqli, $status_service_id, $status_end_time)
+  {
+    $result = $mysqli->query("select * from status, services_status where services_status.service_id = '$status_service_id' AND status.id = services_status.status_id AND status.end_time <= '$status_end_time' AND `end_time` != '0' ORDER BY `status`.`id` DESC limit 1;");
+    $status_row = $result->fetch_row();
+    echo $status_row;
+    if (isset($status_row[0])) {
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -91,31 +102,39 @@ if (!file_exists("../config.php")) {
     $services_status->close();
   }
 
-  /**
-   * Write the Status into DB
-   * @param Objekt $mysqli mysql connection info from config.php
-   * @param int $status_type Error type 0: Major outage 1: Minor outage 2: Planned maintenance 3: Operational
-   * @param int $status_service_id id of the service currently being checked
-   * @param int $status_user_id the User id
-   */
-  function lastOffline($mysqli, $status_type, $status_service_id, $status_user_id)
-  {
-    $result = $mysqli->query("select status.type from status, services_status where services_status.service_id = '$status_service_id' AND status.id = services_status.status_id AND status.user_id = '$status_user_id' ORDER BY `status`.`id` DESC limit 1;");
-    $status_id_type = $result->fetch_row();
-    if (isset($status_id_type[0])) {
-      if ($status_id_type[0] == "$status_type") {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
   $services = $mysqli->query("SELECT * FROM `services` WHERE 'url' IS NOT NULL");
 
   while ($service = $services->fetch_array()) {
     if ("$service[url]") {
-      check("$service[url]", "$service[id]", "$service[name]", $mysqli);
+      $status_id = "$service[id]";
+      $status_name = "$service[name]";
+      list($adresstype, $isonline, $http_code) = check("$service[url]");
+
+      if ($isonline) {
+        $status_text = "Running without Problems";
+        echo "<p style='color:green'>" . $status_id . " " . $status_name;
+        if (lastOffline($mysqli, $status_id, $status_user_id)) {
+          echo " <span style='color:orange'>lastOffline true</span>";
+          writeStatus($mysqli, $status_id, "3", "Online check", $status_text, time(), "0", $status_user_id);
+        }
+        echo "<br>RESPONDE: " . $status_text . "</p>";
+      } else {
+        if ($adresstype == "url") {
+          $http_codes = parse_ini_file("http_codes.ini");
+          $status_text = $http_code . " " . $http_codes[$http_code];
+        } else {
+          $status_text = "Can't reach the Server";
+        }
+        echo "<p style='color:red'>" . $status_id . " " . $status_name;
+        if (!lastOffline($mysqli, $status_id, $status_user_id)) {
+          echo " <span style='color:orange'>lastOffline true</span>";
+          if (!maintenance($mysqli, $status_id, time())) {
+            echo " <span style='color:orange'>maintenance true</span>";
+            writeStatus($mysqli, $status_id, "$status_type_offline", "Online check", $status_text, time(), "0", $status_user_id);
+          }
+        }
+        echo "<br>RESPONDE: " . $status_text . "</p>";
+      }
     }
   }
   $mysqli->close();
