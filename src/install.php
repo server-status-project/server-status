d77d9fa83e138a2ed7b02f002d61a6dd495c11df
Tealk <tealk@anzah.email>
2021-10-07 14:54:35 +0200

    add IMPRINT_URL

--- src/install.php
+++ src/install.php
@@ -3,6 +3,7 @@ require_once("template.php");
 define("WEB_URL", "."); //Website name
 define("NAME", _('Status page')); //Website name
 define("MINIMUM_PHP_VERSION", "5.4.0");
+define("IMPRINT_URL", "policy.php"); //Default policy URL
 define("POLICY_URL", "policy.php"); //Default policy URL
 define("CUSTOM_LOGO_URL", "");
 define("COPYRIGHT_TEXT", "");
@@ -11,9 +12,9 @@ require_once("classes/db-class.php");
 
 $isDeveleoperEnvironement = false;
 if (isset($_GET["isDev"])) {
-  if ($_GET["isDev"] == "devMode") {
-    $isDeveleoperEnvironement = true;
-  }
+    if ($_GET["isDev"] == "devMode") {
+        $isDeveleoperEnvironement = true;
+    }
 }
 
 
@@ -21,152 +22,154 @@ $negotiator = new LocaleNegotiator("en_GB");
 $message = "";
 $db = new SSDB();
 if (!isset($_SESSION['locale']) || isset($_GET['lang'])) {
-  $override = ((isset($_GET['lang'])) ? $_GET['lang'] : null);
-  $best_match = $negotiator->negotiate($override);
-  $_SESSION['locale'] = $best_match;
-  setlocale(LC_ALL, $_SESSION['locale'] . ".UTF-8");
+    $override = ((isset($_GET['lang'])) ? $_GET['lang'] : null);
+    $best_match = $negotiator->negotiate($override);
+    $_SESSION['locale'] = $best_match;
+    setlocale(LC_ALL, $_SESSION['locale'] . ".UTF-8");
 
-  bindtextdomain("server-status", __DIR__ . "/locale/");
-  bind_textdomain_codeset($_SESSION['locale'], "utf-8");
-  textdomain("server-status");
+    bindtextdomain("server-status", __DIR__ . "/locale/");
+    bind_textdomain_codeset($_SESSION['locale'], "utf-8");
+    textdomain("server-status");
 }
 
 if (isset($_POST['server'])) {
-  $mysqli = new mysqli($_POST['server'], $_POST['dbuser'], $_POST['dbpassword'], $_POST['database']);
+    $mysqli = new mysqli($_POST['server'], $_POST['dbuser'], $_POST['dbpassword'], $_POST['database']);
 
-  if ($mysqli->connect_errno) {
-    $message .= sprintf(_("Connection failed: %s\n"), $mysqli->connect_error);
-  }
+    if ($mysqli->connect_errno) {
+        $message .= sprintf(_("Connection failed: %s\n"), $mysqli->connect_error);
+    }
 
-  if (isset($_POST['url']) && filter_var($_POST['url'], FILTER_VALIDATE_URL) === false) {
-    $message .= _("Please set valid url!");
-  }
+    if (isset($_POST['url']) && filter_var($_POST['url'], FILTER_VALIDATE_URL) === false) {
+        $message .= _("Please set valid url!");
+    }
 
     //OstatnÃ­ mÃ¡ checky existence ve funkci pro pridani
-  if (0 == strlen(trim($_POST['servername']))) {
-    $messages[] = _("Server name");
-  }
+    if (0 == strlen(trim($_POST['servername']))) {
+        $messages[] = _("Server name");
+    }
 
-  if (0 == strlen(trim($_POST['url']))) {
-    $messages[] = _("Url");
-  }
+    if (0 == strlen(trim($_POST['url']))) {
+        $messages[] = _("Url");
+    }
 
-  if (0 == strlen(trim($_POST['mailer']))) {
-    $messages[] = _("Mailer name");
-  }
+    if (0 == strlen(trim($_POST['mailer']))) {
+        $messages[] = _("Mailer name");
+    }
 
-  if (0 == strlen(trim($_POST['title']))) {
-    $messages[] = _("Title");
-  }
+    if (0 == strlen(trim($_POST['title']))) {
+        $messages[] = _("Title");
+    }
 
-  if (0 == strlen(trim($_POST['mailer_email']))) {
-    $messages[] = _("Mailer email");
-  }
+    if (0 == strlen(trim($_POST['mailer_email']))) {
+        $messages[] = _("Mailer email");
+    }
 
-  if (0 == strlen(trim($_POST['server']))) {
-    $messages[] = _("Database server");
-  }
+    if (0 == strlen(trim($_POST['server']))) {
+        $messages[] = _("Database server");
+    }
 
-  if (0 == strlen(trim($_POST['database']))) {
-    $messages[] = _("Database name");
-  }
+    if (0 == strlen(trim($_POST['database']))) {
+        $messages[] = _("Database name");
+    }
 
-  if (0 == strlen(trim($_POST['dbuser']))) {
-    $messages[] = _("Database user");
-  }
+    if (0 == strlen(trim($_POST['dbuser']))) {
+        $messages[] = _("Database user");
+    }
 
-  if (0 == strlen(trim($_POST['dbpassword']))) {
-    $messages[] = _("Database password");
-  }
-  if (isset($messages)) {
-    $message .= _("Please enter");
-    $message .= implode(", ", $messages);
-  }
+    if (0 == strlen(trim($_POST['dbpassword']))) {
+        $messages[] = _("Database password");
+    }
+    if (isset($messages)) {
+        $message .= _("Please enter");
+        $message .= implode(", ", $messages);
+    }
 }
 
 if (isset($_POST['server']) && empty($message)) {
-  define("MAILER_NAME", $_POST['mailer']);
-  define("MAILER_ADDRESS", $_POST['mailer_email']);
-  define("INSTALL_OVERRIDE", true);
+    define("MAILER_NAME", $_POST['mailer']);
+    define("MAILER_ADDRESS", $_POST['mailer_email']);
+    define("INSTALL_OVERRIDE", true);
 
     //No need to include config, as we have connection from testing it... :)
     //There may be better way to do this...
-  $sql = file_get_contents("install.sql");
-  $array = explode(";", $sql);
+    $sql = file_get_contents("install.sql");
+    $array = explode(";", $sql);
 
-  foreach ($array as $value) {
-    $val = trim($value);
-    if (empty($val)) {
-      continue;
+    foreach ($array as $value) {
+        $val = trim($value);
+        if (empty($val)) {
+            continue;
+        }
+        $q_res = $mysqli->query($value);
+        if ($q_res === false) {
+            $message = sprintf(_("Error while creating database. Please check permission for your account or MYSQL version.<br>Error: %s"), $mysqli->error);
+            break;
+        }
     }
-    $q_res = $mysqli->query($value);
-    if ($q_res === false) {
-      $message = sprintf(_("Error while creating database. Please check permission for your account or MYSQL version.<br>Error: %s"), $mysqli->error);
-      break;
+
+    if (empty($message)) {
+        require_once("classes/constellation.php");
+
+        User::add();
     }
-  }
 
-  if (empty($message)) {
-    require_once("classes/constellation.php");
-
-    User::add();
-  }
-
-  if (empty($message)) {
+    if (empty($message)) {
         //Create config
-    $config = file_get_contents("config.php.template");
+        $config = file_get_contents("config.php.template");
         //$config = str_replace("##name##", htmlspecialchars($_POST['servername'], ENT_QUOTES), $config);
-    $db->setSetting($mysqli, "name", htmlspecialchars($_POST['servername'], ENT_QUOTES));
+        $db->setSetting($mysqli, "name", htmlspecialchars($_POST['servername'], ENT_QUOTES));
         //$config = str_replace("##title##", htmlspecialchars($_POST['title'], ENT_QUOTES), $config);
-    $db->setSetting($mysqli, "title", htmlspecialchars($_POST['title'], ENT_QUOTES));
+        $db->setSetting($mysqli, "title", htmlspecialchars($_POST['title'], ENT_QUOTES));
         //$config = str_replace("##url##", $_POST['url'], $config);
-    $db->setSetting($mysqli, "url", $_POST['url']);
+        $db->setSetting($mysqli, "url", $_POST['url']);
         //$config = str_replace("##mailer##", htmlspecialchars($_POST['mailer'], ENT_QUOTES), $config);
-    $db->setSetting($mysqli, "mailer", htmlspecialchars($_POST['mailer'], ENT_QUOTES));
+        $db->setSetting($mysqli, "mailer", htmlspecialchars($_POST['mailer'], ENT_QUOTES));
         //$config = str_replace("##mailer_email##", htmlspecialchars($_POST['mailer_email'], ENT_QUOTES), $config);
-    $db->setSetting($mysqli, "mailer_email", htmlspecialchars($_POST['mailer_email'], ENT_QUOTES));
-    $config = str_replace("##server##", htmlspecialchars($_POST['server'], ENT_QUOTES), $config);
-    $config = str_replace("##database##", htmlspecialchars($_POST['database'], ENT_QUOTES), $config);
-    $config = str_replace("##user##", htmlspecialchars($_POST['dbuser'], ENT_QUOTES), $config);
-    $config = str_replace("##password##", htmlspecialchars($_POST['dbpassword'], ENT_QUOTES), $config);
+        $db->setSetting($mysqli, "mailer_email", htmlspecialchars($_POST['mailer_email'], ENT_QUOTES));
+        $config = str_replace("##server##", htmlspecialchars($_POST['server'], ENT_QUOTES), $config);
+        $config = str_replace("##database##", htmlspecialchars($_POST['database'], ENT_QUOTES), $config);
+        $config = str_replace("##user##", htmlspecialchars($_POST['dbuser'], ENT_QUOTES), $config);
+        $config = str_replace("##password##", htmlspecialchars($_POST['dbpassword'], ENT_QUOTES), $config);
         // Duplicate of lines 122-123 //$config = str_replace("##name##", htmlspecialchars($_POST['servername'], ENT_QUOTES), $config);
-    $config = str_replace("##policy_name##", htmlspecialchars($_POST['policy_name'], ENT_QUOTES), $config);
-    $config = str_replace("##address##", htmlspecialchars($_POST['address'], ENT_QUOTES), $config);
-    $config = str_replace("##policy_mail##", htmlspecialchars($_POST['policy_mail'], ENT_QUOTES), $config);
-    $config = str_replace("##policy_phone##", htmlspecialchars($_POST['policy_phone'], ENT_QUOTES), $config);
-    $config = str_replace("##who_we_are##", htmlspecialchars($_POST['who_we_are'], ENT_QUOTES), $config);
-    $policy_url_conf = (!empty($_POST['policy_url'])) ? htmlspecialchars($_POST['policy_url'], ENT_QUOTES) : $_POST['url'] . "/policy.php";
-    $config = str_replace("##policy_url##", $policy_url_conf, $config);
+        $config = str_replace("##policy_name##", htmlspecialchars($_POST['policy_name'], ENT_QUOTES), $config);
+        $config = str_replace("##address##", htmlspecialchars($_POST['address'], ENT_QUOTES), $config);
+        $config = str_replace("##policy_mail##", htmlspecialchars($_POST['policy_mail'], ENT_QUOTES), $config);
+        $config = str_replace("##policy_phone##", htmlspecialchars($_POST['policy_phone'], ENT_QUOTES), $config);
+        $config = str_replace("##who_we_are##", htmlspecialchars($_POST['who_we_are'], ENT_QUOTES), $config);
+        $policy_url_conf = (!empty($_POST['policy_url'])) ? htmlspecialchars($_POST['policy_url'], ENT_QUOTES) : $_POST['url'] . "/policy.php";
+        $config = str_replace("##policy_url##", $policy_url_conf, $config);
+        $imprint_url_conf = (!empty($_POST['imprint_url'])) ? htmlspecialchars($_POST['imprint_url'], ENT_QUOTES) : $_POST['url'] . "/policy.php";
+        $config = str_replace("##imprint_url##", $imprint_url_conf, $config);
 
-    file_put_contents("config.php", $config);
+        file_put_contents("config.php", $config);
 
-    include_once "create-server-config.php";
-    $db->setSetting($mysqli, "dbConfigVersion", "Version2Beta7");
-    $db->setSetting($mysqli, "notifyUpdates", "yes");
-    $db->setSetting($mysqli, "subscribe_email", "no");
-    $db->setSetting($mysqli, "subscribe_telegram", "no");
-    $db->setSetting($mysqli, "tg_bot_api_token", "");
-    $db->setSetting($mysqli, "tg_bot_username", "");
-    $db->setSetting($mysqli, "php_mailer", "no");
-    $db->setSetting($mysqli, "php_mailer_host", "");
-    $db->setSetting($mysqli, "php_mailer_smtp", "no");
-    $db->setSetting($mysqli, "php_mailer_path", "");
-    $db->setSetting($mysqli, "php_mailer_port", "");
-    $db->setSetting($mysqli, "php_mailer_secure", "no");
-    $db->setSetting($mysqli, "php_mailer_user", "");
-    $db->setSetting($mysqli, "php_mailer_pass", "");
-    $db->setSetting($mysqli, "google_recaptcha", "no");
-    $db->setSetting($mysqli, "google_recaptcha_secret", "");
-    $db->setSetting($mysqli, "google_recaptcha_sitekey", "");
-    $db->setSetting($mysqli, "cron_server_ip", "");
-    if (!$isDeveleoperEnvironement) {
-      unlink("create-server-config.php");
-      unlink("config.php.template");
-      unlink("install.sql");
-      unlink(__FILE__);
+        include_once "create-server-config.php";
+        $db->setSetting($mysqli, "dbConfigVersion", "Version2Beta7");
+        $db->setSetting($mysqli, "notifyUpdates", "yes");
+        $db->setSetting($mysqli, "subscribe_email", "no");
+        $db->setSetting($mysqli, "subscribe_telegram", "no");
+        $db->setSetting($mysqli, "tg_bot_api_token", "");
+        $db->setSetting($mysqli, "tg_bot_username", "");
+        $db->setSetting($mysqli, "php_mailer", "no");
+        $db->setSetting($mysqli, "php_mailer_host", "");
+        $db->setSetting($mysqli, "php_mailer_smtp", "no");
+        $db->setSetting($mysqli, "php_mailer_path", "");
+        $db->setSetting($mysqli, "php_mailer_port", "");
+        $db->setSetting($mysqli, "php_mailer_secure", "no");
+        $db->setSetting($mysqli, "php_mailer_user", "");
+        $db->setSetting($mysqli, "php_mailer_pass", "");
+        $db->setSetting($mysqli, "google_recaptcha", "no");
+        $db->setSetting($mysqli, "google_recaptcha_secret", "");
+        $db->setSetting($mysqli, "google_recaptcha_sitekey", "");
+        $db->setSetting($mysqli, "cron_server_ip", "");
+        if (!$isDeveleoperEnvironement) {
+            unlink("create-server-config.php");
+            unlink("config.php.template");
+            unlink("install.sql");
+            unlink(__FILE__);
+        }
+        header("Location: " . WEB_URL);
     }
-    header("Location: " . WEB_URL);
-  }
 }
 Template::render_header(_("Install"), "install");
 
@@ -180,7 +183,7 @@ $preq_writedir = $preq_fail;
 
 // Check if PHP version if > MINIMUM_PHP_VERSION
 if (strnatcmp(phpversion(), MINIMUM_PHP_VERSION) >= 0) {
-  $preq_phpver = $preq_ok;
+    $preq_phpver = $preq_ok;
 }
 
 // Test for mysqlnd precense.  The mysqlnd driver provides some extra functions that is not available
@@ -188,12 +191,12 @@ if (strnatcmp(phpversion(), MINIMUM_PHP_VERSION) >= 0) {
 // on the PHP site at http://www.php.net/manual/en/mysqlnd.stats.php
 // This test is also discussed at https://stackoverflow.com/questions/1475701/how-to-know-if-mysqlnd-is-the-active-driver
 if (function_exists('mysqli_get_client_stats')) {
-  $preq_mysqlnd = $preq_ok;
+    $preq_mysqlnd = $preq_ok;
 }
 
 // Check if we have access to write to location
 if (is_writable(__DIR__)) {
-  $preq_writedir = $preq_ok;
+    $preq_writedir = $preq_ok;
 }
 
 ?>
@@ -227,9 +230,9 @@ if (is_writable(__DIR__)) {
         <h1 class="text-center"><?php echo _("Installation"); ?></h1>
         <?php
         if (!empty($message)) {
-          ?>
+        ?>
             <p class="alert alert-danger"><?php echo $message; ?></p>
-          <?php
+        <?php
         }
         ?>
         <span><?php echo _("We will ask you some basic questions about your website. Most of the settings can be later edited in the config.php file."); ?></span>
@@ -328,6 +331,13 @@ if (is_writable(__DIR__)) {
                         <div class="col form-floating">
                             <textarea class="form-control" id="who_we_are" rows="3" name="who_we_are" placeholder="<?php echo _("Some info about yourself"); ?>" value="<?php echo ((isset($_POST['who_we_are'])) ? htmlspecialchars($_POST['who_we_are'], ENT_QUOTES) : ''); ?>"></textarea>
                             <label for="who_we_are"><?php echo _("Who we are"); ?>: </label>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <span><?php echo _("If you alredy have an existing Imprint published, please provide the full Url to override the local policy definition. Leave blank to use the local definition"); ?></span>
+                        <div class="col form-floating">
+                            <input type="imprint_url" name="imprint_url" value="<?php echo ((isset($_POST['imprint_url'])) ? htmlspecialchars($_POST['imprint_url'], ENT_QUOTES) : ''); ?>" id="imprint_url" placeholder="<?php echo _("External Imprint Url"); ?>" class="form-control">
+                            <label for="url"><?php echo _("External Imprint Url"); ?>: </label>
                         </div>
                     </div>
                     <div class="row mt-3">
