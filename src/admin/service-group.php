72be543277abb6f6b4a60bc27fc0eb1b47cf9326
Tealk <tealk@anzah.email>
2021-10-07 14:42:50 +0200

    use new bootstrap style

--- src/admin/service-group.php
+++ src/admin/service-group.php
@@ -32,59 +32,57 @@ if (isset($_GET['id']) && !isset($_POST['id'])) {
 
 if (!$boolEdit) {
   Template::render_header(_('New service group'), 'service-group', true); ?>
-<div class="text-center">
+  <div class="text-center">
     <h2><?php echo _('Add new service group'); ?></h2>
-</div>
-  <?php
-  $form_url = WEB_URL.'/admin/?do=new-service-group&amp;new=group';
+  </div>
+<?php
+  $form_url = WEB_URL . '/admin/?do=new-service-group&amp;new=group';
 } else {
   Template::render_header(_('Edit service group'), 'service-group', true);
-  ?>
-    <div class="text-center">
-        <h2><?php echo _('Edit service group'); ?></h2>
-    </div>
-  <?php
-  $form_url = WEB_URL.'/admin/?do=edit-service-group&amp;edit&amp;id='.$group_id;
+?>
+  <div class="text-center">
+    <h2><?php echo _('Edit service group'); ?></h2>
+  </div>
+<?php
+  $form_url = WEB_URL . '/admin/?do=edit-service-group&amp;edit&amp;id=' . $group_id;
 }
 ?>
 
 <form action="<?php echo $form_url; ?>" method="POST" class="form-horizontal">
-    <?php if (isset($message)) { ?>
-      <p class="alert alert-danger"><?php echo $message; ?></p>
-      <?php
-    }
-    ?>
-    <div class="row">
-        <div class="col-sm-6"><label for="group"><?php echo _('Service Group Name'); ?>: </label><input type="text" maxlength="50" name="group" value="<?php echo (isset($_POST['group'])) ? htmlspecialchars($_POST['group'], ENT_QUOTES) : $group_value; ?>" id="group" placeholder="<?php echo _('service group name'); ?>" class="form-control" required></div>
-        <div class="col-sm-6"><label for="description"><?php echo _('Description'); ?>: </label><input type="text" maxlength="100" name="description" value="<?php echo (isset($_POST['description'])) ? htmlspecialchars($description_value, ENT_QUOTES) : $description_value; ?>" id="description" placeholder="<?php echo _('Description'); ?>" class="form-control"></div>
+  <?php if (isset($message)) { ?>
+    <p class="alert alert-danger"><?php echo $message ?></p>
+  <?php
+  } ?>
+  <div class="form-group">
+    <div class="col-sm-6"><label for="group"><?php echo _("Service Group Name"); ?>: </label><input type="text" maxlength="50" name="group" value="<?php echo ((isset($_POST['group'])) ? htmlspecialchars($_POST['group'], ENT_QUOTES) : $group_value); ?>" id="group" placeholder="<?php echo _("service group name"); ?>" class="form-control" required></div>
+    <div class="col-sm-6"><label for="description"><?php echo _("Description"); ?>: </label><input type="text" maxlength="100" name="description" value="<?php echo ((isset($_POST['description'])) ? htmlspecialchars($description_value, ENT_QUOTES) : $description_value); ?>" id="description" placeholder="<?php echo _("Description"); ?>" class="form-control"></div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-6">
+      <label for="visibility_id"><?php echo _("Visibility"); ?>: </label>
+      <select name="visibility_id" id="visibility_id" class="form-control">
+        <?php
+        if (!empty($visibility_id_value)) {
+          $visibility_id = $visibility_id_value;
+        } else {
+          $visibility_id = null;
+        }
+        //$visibilitys = Service::get_groups();
+        foreach ($visibility as $key => $value) {
+          if ($visibility_id == $key) {
+            echo '<option value="' . $key . '" selected>' . $value . '</option>';
+          } else {
+            echo '<option value="' . $key . '">' . $value . '</option>';
+          }
+        }
+        ?>
+      </select>
     </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <label for="visibility_id"><?php echo _('Visibility'); ?>: </label>
-            <select name="visibility_id" id="visibility_id" class="form-select">
-                <?php
-                if (!empty($visibility_id_value)) {
-                  $visibility_id = $visibility_id_value;
-                } else {
-                  $visibility_id = null;
-                }
-
-                // $visibilitys = Service::get_groups();
-                foreach ($visibility as $key => $value) {
-                  if ($visibility_id == $key) {
-                    echo '<option value="'.$key.'" selected>'.$value.'</option>';
-                  } else {
-                    echo '<option value="'.$key.'">'.$value.'</option>';
-                  }
-                }
-                ?>
-            </select>
-        </div>
-    </div>
-    <?php
-    if ($boolEdit) {
-      echo '<input type="hidden" id="id" name="id" value="'.$group_id.'">';
-    }
-    ?>
-    <button type="submit" class="btn btn-primary float-end"><?php echo _('Submit'); ?></button>
+  </div>
+  <?php
+  if ($boolEdit) {
+    echo '<input type="hidden" id="id" name="id" value="' . $group_id . '">';
+  }
+  ?>
+  <button type="submit" class="btn btn-primary float-end"><?php echo _("Submit"); ?></button>
 </form>
--- src/template.php
+++ src/template.php
@@ -10,45 +10,46 @@ $visibility = array(_("Collapsed"), _("Expanded"), _("Expand on events"));
 /**
  * Class that encapsulates methods to render header and footer
  */
-class Template {
+class Template
+{
   /**
    * Renders header
    * @param String $page_name name of the page to be displayed as title
    * @param Boolean $admin decides whether to show admin menu
    */
-  public static function render_header($page_name, $page_id, $admin = false) {
-    $strSubsMenu = '';
+  public static function render_header($page_name, $page_id, $admin = false)
+  {
     if (!$admin) {
       // Create subscriber menu sections for later inclusion
       // Check if we are on admin menu, if so do not display
       $arr_url = explode("/", $_SERVER['PHP_SELF']);
       $str_url = strtolower($arr_url[count($arr_url) - 2]);
-      if ('admin' != $str_url) {
-        if ((defined('SUBSCRIBE_EMAIL') && SUBSCRIBE_EMAIL) || (defined('SUBSCRIBE_TELEGRAM') && SUBSCRIBE_TELEGRAM)) {
+      $strSubsMenu = '';
+      if ('admin' == $str_url) {
+      } else {
+        if (SUBSCRIBE_EMAIL || SUBSCRIBE_TELEGRAM) {
           // Subscriber menu is to be shown...
-        $strSubsMenu = '<ul class="nav navbar-nav mr-auto">';
+          $strSubsMenu = '<li class="nav-item dropdown">';
           // If subscriber is not logged on, display subscriber menus
-        if ((!isset($_SESSION['subscriber_valid'])) || false == $_SESSION['subscriber_valid']) {
-          $strSubsMenu .= '<li class="dropdown">
-                                  <a class="dropdown-toggle" data-bs-toggle="dropdown" role="button" href="#"><span class="bi bi--grid-3x3-gap-fill"></span>&nbsp;' . _('Subscribe') . '</a>
-                                  <ul class="dropdown-menu ">';
+          if ((!isset($_SESSION['subscriber_valid'])) || false == $_SESSION['subscriber_valid']) {
+            $strSubsMenu .= '<a class="nav-link dropdown-toggle" href="#" id="subscriberDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">' . _('Subscribe') . '</a>
+                             <ul class="dropdown-menu" aria-labelledby="subscriberDropdown">';
 
-          if (SUBSCRIBE_EMAIL) {
-            $strSubsMenu .= '<li><a class="dropdown-item" href="?do=email_subscription&amp;new=1"><span class="bi bi-envelope"></span>&nbsp;' . _('Subscribe via email') . '</a></li>';
-          }
-          if (SUBSCRIBE_TELEGRAM) {
-            $strSubsMenu .= '<li><a class="dropdown-item" href="#"><script async src="https://telegram.org/js/telegram-widget.js?4" data-telegram-login="' . TG_BOT_USERNAME . '" data-size="small" data-userpic="false" data-auth-url="' . WEB_URL . '/telegram_check.php" data-request-access="write"></script></a></li>';
+            if (SUBSCRIBE_EMAIL) {
+              $strSubsMenu .= '<li><a class="dropdown-item" href="?do=email_subscription&amp;new=1"><span class="glyphicon glyphicon-envelope"></span>&nbsp;' . _('Subscribe via email') . '</a></li>';
+            }
+            if (SUBSCRIBE_TELEGRAM) {
+              $strSubsMenu .= '<li><a class="dropdown-item" href="#"><script async src="https://telegram.org/js/telegram-widget.js?4" data-telegram-login="' . TG_BOT_USERNAME . '" data-size="small" data-userpic="false" data-auth-url="' . WEB_URL . '/telegram_check.php" data-request-access="write"></script></a></li>';
+            }
+            $strSubsMenu .=  '</ul>';
           }
         }
-      }
-       // If subscriber is logged on, display unsub and logoff menu points
-      if ((isset($_SESSION['subscriber_valid'])) &&  $_SESSION['subscriber_valid']) {
-        $strSubsMenu .= '<li><a href="?do=subscriptions">' . _('Subscriptions') . '</a></li>';
-        $strSubsMenu .= '<li><a href="' . WEB_URL . '/index.php?subscriber_logout=1">' . _('Logout') . '</a></li>';
-      }
-      if (!empty($strSubsMenu)){
-        $strSubsMenu .=  '</ul>';
-      }
+        // If subscriber is logged on, display unsub and logoff menu points
+        if ((isset($_SESSION['subscriber_valid'])) &&  $_SESSION['subscriber_valid']) {
+          $strSubsMenu .= '<li><a href="?do=subscriptions">' . _('Subscriptions') . '</a></li>';
+          $strSubsMenu .= '<li><a href="' . WEB_URL . '/index.php?subscriber_logout=1">' . _('Logout') . '</a></li>';
+        }
+        $strSubsMenu .=  '</li>';
       }
     }
 ?>
@@ -81,79 +82,77 @@ class Template {
       <meta name="msapplication-TileColor" content="#ffffff">
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
       <meta name="theme-color" content="#ffffff">
-      <link href="<?php echo WEB_URL; ?>/vendor/fontawesome/css/all.min.css" rel="stylesheet">
+      <link href="/vendor/fontawesome/css/all.min.css" rel="stylesheet">
       <?php
-      if (!$admin) {
-        $headpath = $_SERVER['DOCUMENT_ROOT'] . "/head.txt";
-        $headfile = fopen("$headpath", "r") or die("Unable to open head.txt!");
-        $head_additionalcode = fread($headfile, filesize($headpath));
-        fclose($headfile);
-        echo $head_additionalcode;
-      } else {
+      if ($admin) {
         global $user;
       ?>
-        <link rel="stylesheet" href="<?php echo WEB_URL; ?>/css/flatpickr.min.css">
+        <link rel="stylesheet" href="<?php echo WEB_URL; ?>/vendor/flatpickr/flatpickr.min.css">
       <?php
       }
       ?>
     </head>
 
     <body>
-      <nav class="navbar navbar-expand-lg navbar-default">
+      <header class="fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="<?php echo WEB_URL; ?>"><img src="<?php
-                                                                          if (strlen(CUSTOM_LOGO_URL) > 1) {
-                                                                            echo CUSTOM_LOGO_URL;
-                                                                          } else {
-                                                                            echo WEB_URL . "/img/logo_white.png";
-                                                                          } ?>" alt="logo" class="menu-logo" style="height:50px;">
-            <h1 class="ms-3"><?php echo _((defined('TITLE') ? TITLE : "Service Status")); ?></h1>
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarToggler">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <?php
-              if (!$admin) {
-              ?>
-                <li class="d-flex">
-                  <button class="nav-link" id="theme-toggle" accesskey="t" title="(Alt + T)" onclick="themeToggle()"></button>
-                </li>
-                <li class="nav-item">
-                </li>
-              <?php
-              } else {
-                global $user;
-              ?>
-                <li class="nav-item">
-                  <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/"><?php echo _("Dashboard"); ?></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=user"><?php printf(_("User (%s)"), $user->get_username()); ?></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=settings"><?php echo _("Services & Users"); ?></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=options"><?php echo _("Options"); ?></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=logout"><?php echo _("Logout"); ?></a>
-                </li>
-                <li class="d-flex">
-                  <button class="nav-link" id="theme-toggle" accesskey="t" title="(Alt + T)" onclick="themeToggle()"></button>
-                </li>
-              <?php
-              }
-              ?>
-            </ul>
-            <?php echo $strSubsMenu; ?>
-          </div>
+          <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="<?php echo WEB_URL; ?>/"><img src="<?php if (strlen(CUSTOM_LOGO_URL) > 1) {
+                                                                                  echo CUSTOM_LOGO_URL;
+                                                                                } else {
+                                                                                  echo WEB_URL . "/img/logo_white.png";
+                                                                                } ?>" alt="logo" class="menu-logo" width="50" height="50"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <?php
+                  if (!$admin) {
+                  ?>
+                    <li class="nav-item">
+                      <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/"><?php echo _("Admin"); ?></a>
+                    </li>
+                    <?php echo $strSubsMenu; ?>
+                    <li class="d-flex">
+                      <button class="nav-link" id="theme-toggle" accesskey="t" title="(Alt + T)" onclick="themeToggle()"></button>
+                    </li>
+                  <?php
+                  } else {
+                    global $user;
+                  ?>
+                    <li class="nav-item">
+                      <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/"><?php echo _("Dashboard"); ?></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=user"><?php printf(_("User (%s)"), $user->get_username()); ?></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=settings"><?php echo _("Services & Users"); ?></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=options"><?php echo _("Options"); ?></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link link-light" href="<?php echo WEB_URL; ?>/admin/?do=logout"><?php echo _("Logout"); ?></a>
+                    </li>
+                    <li class="d-flex">
+                      <button class="nav-link" id="theme-toggle" accesskey="t" title="(Alt + T)" onclick="themeToggle()"></button>
+                    </li>
+                  <?php
+                  }
+                  ?>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </div>
-      </nav>
-      <main id="<?php echo $page_id; ?>" class="container wrapper<?php echo $admin ? ' admin' : ''?>">
-        <?php
+      </header>
+      <main id="<?php echo $page_id; ?>" class="container <?php if ($admin) { ?>admin<?php } ?>">
+        <?php if ($admin) {
+          global $user;
+        }
       }
       /**
        * Renders a toggle switch
@@ -165,51 +164,53 @@ class Template {
       public static function render_toggle($toggletext, $input_name, $checked)
       {
         ?>
-              <div>
-              <h3><?php echo $toggletext; ?> 
-                <label class="switch d-block float-end">
-                    <input type="checkbox" name="<?php echo $input_name; ?>" <?php if ($checked) {
-                      echo "checked";
-                                                 } ?> >
-                    <span class="slider round"></span>
-                </label>
-              </h3>
-              </div>
-        <?php
+        <div id="switch">
+          <label class="switch">
+            <input type="checkbox" name="<?php echo $input_name; ?>" <?php if ($checked) {
+                                                                        echo "checked";
+                                                                      } ?>>
+            <span class="slider round"></span>
+          </label>
+          <?php echo $toggletext; ?>
+        </div>
+      <?php
       }
       /**
        * Renders footer
        * @param Boolean $admin decides whether to load admin scripts
        */
-      public static function render_footer($admin = false) {
+      public static function render_footer($admin = false)
+      {
         global $negotiator;
         $lang_names = $negotiator->get_accepted_langs();
       ?>
       </main>
-      <footer id="footerwrap">
-        <div class="container pb-3">
+      <footer id="footerwrap" class="sticky-bottom">
+        <div class="container">
           <div class="row centered">
             <div class="col-md-4 text-left"><a class="link-light" href="https://github.com/server-status-project/server-status/graphs/contributors" target=”_blank” rel=”noopener noreferrer”>Copyright © <?php echo date("Y"); ?> Server Status Project Contributors </a><?php if (strlen(COPYRIGHT_TEXT) > 1) {
                                                                                                                                                                                                                                                                             echo " and " . COPYRIGHT_TEXT;
                                                                                                                                                                                                                                                                           } ?></div>
             <div class="col-md-4 text-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-primary"><?php echo '<img src="' . WEB_URL . '/locale/' . $_SESSION['locale'] . '/flag.png" alt="' . $lang_names[$_SESSION['locale']] . '">' . $lang_names[$_SESSION['locale']]; ?></button>
+              <div class="input-group dropup mb-3">
+                <button type="button" class="btn btn-primary" disabled><?php echo '<img src="' . WEB_URL . '/locale/' . $_SESSION['locale'] . '/flag.png" alt="' . $lang_names[$_SESSION['locale']] . '">' . $lang_names[$_SESSION['locale']]; ?></button>
                 <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                   <span class="visually-hidden"><?php echo _("Toggle Dropdown"); ?></span>
                 </button>
-                <div class="dropdown-menu">
+                <ul class="dropdown-menu">
                   <?php
                   foreach ($lang_names as $key => $value) {
-                    echo '<a href="?lang=' . $key . '" class="dropdown-item"><img src="' . WEB_URL . '/locale/' . $key . '/flag.png" alt="' . $value . '">' . $value . '</a>';
+                    echo '<a href="?lang=' . $key . '"><img src="' . WEB_URL . '/locale/' . $key . '/flag.png" alt="' . $value . '">' . $value . '</a>';
                   }
                   ?>
-                  <hr class="dropdown-divider">
-                  <a href="https://poeditor.com/join/project/37SpmJtyOm" target=”_blank” rel=”noopener noreferrer”><?php echo _("Help with translation!"); ?></a>
-                </div>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="https://poeditor.com/join/project/37SpmJtyOm" target=”_blank” rel=”noopener noreferrer”><?php echo _("Help with translation!"); ?></a></li>
+                </ul>
               </div>
             </div>
-            <div class="col-md-4 text-right"><a class="link-light me-3" href="<?php echo IMPRINT_URL; ?>"><?php echo _("Imprint"); ?></a><a class="link-light" href="<?php echo POLICY_URL; ?>"><?php echo _("Privacy Policy"); ?></a></div>
+            <div class="col-md-4 text-right link-light"><a class="link-light" href="<?php echo IMPRINT_URL; ?>"><?php echo _("Imprint"); ?></a> - <a class="link-light" href="<?php echo POLICY_URL; ?>"><?php echo _("Privacy Policy"); ?></a></div>
           </div>
           <!--/row -->
         </div>
@@ -217,16 +218,14 @@ class Template {
       </footer>
       <script src="<?php echo WEB_URL; ?>/vendor/jquerry/jquery-3.6.0.min.js"></script>
       <script src="<?php echo WEB_URL; ?>/vendor/jquerry/jquery.timeago.js"></script>
-      <script src="<?php echo WEB_URL; ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+      <script src="<?php echo WEB_URL; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
       <script src="<?php echo WEB_URL; ?>/js/main.js"></script>
       <script src="<?php echo WEB_URL; ?>/js/themeToggle.js"></script>
       <?php if ($admin) { ?>
         <script src="<?php echo WEB_URL; ?>/vendor/flatpickr/flatpickr.min.js"></script>
         <script src="<?php echo WEB_URL; ?>/js/admin.js"></script>
       <?php } ?>
-      <?php if (GOOGLE_RECAPTCHA) {
-      ?><script src='https://www.google.com/recaptcha/api.js'></script><?php
-                                                                      } ?>
+      <?php if (GOOGLE_RECAPTCHA) { ?><script src='https://www.google.com/recaptcha/api.js'></script><?php } ?>
     </body>
 
     </html>
